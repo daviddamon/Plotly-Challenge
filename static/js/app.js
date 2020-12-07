637e2b0aae33b5c8310caecd8a9e7afe3e55481a
Dave Damon <dave@daviddamon.com>
2020-12-06 22:09:03 -0500

    re-crreated repo and added gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,24 @@
 // Plotly Challenge
 
-// get data from json to build plots
+// create function to build plots
 function buildPlots(id) {
 
   // load json file into json object
   d3.json("samples.json").then((data) => {
     console.log(data)
 
-    // ***** Bar Chart *****
-    // Use sample_values as the values for the bar chart.
-    // Use otu_ids as the labels for the bar chart.
-    // Use otu_labels as the hovertext for the chart.
-
+    // prepare data for charts
     // filter metadata by id
     var samples = data.samples.filter(s => s.id === id)[0];
-    //console.log(samples)
+    console.log(samples)
     
     // get top 10 sample values
     var top10SampleValues = samples.sample_values.slice(0, 10).reverse();
-    //console.log(top10SampleValues);
+    console.log(top10SampleValues);
    
     // get top 10 otu ids
     var top10OtuIDs = samples.otu_ids.slice(0, 10).reverse();
-    //console.log(top10OtuIDs);
+    console.log(top10OtuIDs);
 
     // change the top 10 otu id's to the required format
     var idOtu = top10OtuIDs.map(d => "OTU " + d);
@@ -33,7 +29,16 @@ function buildPlots(id) {
     console.log(`Top 10 Sample Values: ${top10SampleValues}`);
     console.log(`Top 10 Id Values: ${top10OtuIDs}`);
 
-    // create a trace for bar chart - from activity 1.02
+    // get wash frequency
+    var metaData = data.metadata;
+    var result = metaData.filter(meta => meta.id == id)[0];
+    console.log(result)  
+    var washFrequency = result.wfreq;
+    console.log(washFrequency);
+
+   
+    // ***** Bar Chart *****
+    // create a trace for bar chart
     var trace1 = {
       type:"bar",
       orientation: "h",
@@ -68,13 +73,8 @@ function buildPlots(id) {
     // Plot the chart to a div tag with id "bar"
     Plotly.newPlot("bar", data1, layout1);
 
-    // ***** Bubble Chart *****
-    // Use otu_ids for the x values.
-    // Use sample_values for the y values.
-    // Use sample_values for the marker size.
-    // Use otu_ids for the marker colors.
-    // Use otu_labels for the text values.
 
+    // ***** Bubble Chart *****
     // create trace2 for bubble chart
     var trace2 = {
       type: "bubble",
@@ -85,7 +85,10 @@ function buildPlots(id) {
       marker: {
         size: samples.sample_values,
         color: samples.otu_ids,
-        colorscale: "Jet"
+        colorscale: "Jet",
+        sizeref: 2.0 * Math.max(samples.sample_values) / (40**2),
+        // sizemode: 'area',
+        sizemin: 5
       }
     };
       
@@ -93,85 +96,72 @@ function buildPlots(id) {
 
     var layout2 = {
       title: "OTU ID vs. Count",
-      showlegend: true,
-      height: 500,
-      width: 1000
+      xaxis: { title: "OTU ID"},
+			yaxis: { title: "Sample Count"}, 
+      showlegend: false,
+      height: 800,
+      width: 1200
     };
 
     Plotly.newPlot("bubble", data2, layout2);
-  
+
 
     // ***** Gauge Chart *****
-    // Adapt the Gauge Chart from <https://plot.ly/javascript/gauge-charts/> to plot the weekly washing frequency of the individual.
-    // You will need to modify the example gauge code to account for values ranging from 0 through 9.
-    // Update the chart whenever a new sample is selected.
-
-    // // get wash frequency
-    // var washFrequency = data.metadata.wfreq;
-    // console.log(washFrequency);
-
-    // var trace3 = [
-    //   {
-    //     domain: {x:[0,1], y:[0,1]},
-    //     type: "indicator",
-    //     mode: "gauge",
-    //     value: washFrequency,
-    //     title: { text: "Belly Button Washes Per Week", color: "#444444", font: { size: 24 } },
-    //     gauge: {
-    //       shape: "angular",//pointer
-    //       axis: { range: [null, 9]},
-    //       bar: { color: "#850000" },//pointer color
-    //       bgcolor: "#ffffff",//gauge background color
-    //       //borderwidth: 2,
-    //       //bordercolor: "gray",
-    //       steps: [
-    //         { range: [0, 1], color: "#f8f3ec" },
-    //         { range: [1, 2], color: "#f4f1e4" },
-    //         { range: [2, 3], color: "#e9e6c9" },
-    //         { range: [3, 4], color: "#e5e8b0" },
-    //         { range: [4, 5], color: "#d5e599" },
-    //         { range: [5, 6], color: "#b7cd8f" },
-    //         { range: [6, 7], color: "#8ac086" },
-    //         { range: [7, 8], color: "#89bc8d" },
-    //         { range: [8, 9], color: "#84b589" }
-    //       ],
-    //       threshold: {
-    //         line: { color: "#850000", width: 4 },
-    //         thickness: 0.75,
-    //         value: 9
-    //       }
-    //     }
-    //   }
-    // ];
+    // create trace 3 for gauge chart
+    var trace3 = [
+      {
+        domain: {x: [0,1], y: [0,1]},        
+        type: "indicator",
+        mode: "gauge",
+        value: washFrequency,
+        title: { text: "<b>Belly Button Washing Frequency</b><br><i>Scrubs per Week</i>", color: "#444444", font: { size: 24 } },
+        gauge: {
+          shape: "angular",//pointer
+          axis: { range: [0, 9]},
+          bar: { color: "#850000" },//pointer color
+          bgcolor: "#ffffff",//gauge background color         
+          steps: [
+            { range: [0, 1], color: "#f8f3ec" },
+            { range: [1, 2], color: "#f4f1e4" },
+            { range: [2, 3], color: "#e9e6c9" },
+            { range: [3, 4], color: "#e5e8b0" },
+            { range: [4, 5], color: "#d5e599" },
+            { range: [5, 6], color: "#b7cd8f" },
+            { range: [6, 7], color: "#8ac086" },
+            { range: [7, 8], color: "#89bc8d" },
+            { range: [8, 9], color: "#84b589" }
+          ],
+          threshold: {
+            line: { color: "#850000", width: 4 },
+            thickness: 0.75,
+            value: 9
+          }
+        }
+      }
+    ];
     
-    // var data3 = [trace3];
+    var data3 = [trace3];
 
-    // var layout3 = {
-    //   title: "Belly Button Washing Frequency",
-    //   width: 500,
-    //   height: 400,
-    //   margin: { t: 25, r: 25, l: 25, b: 25 },
-    //   //paper_bgcolor: "#ffffff"",
-    //   //font: { color: "black", family: "Arial" }
-    // };
+    var layout3 = {
+      // title: "Belly Button Washing Frequency",
+      width: 600,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      //paper_bgcolor: "#ffffff"",
+      font: { color: "#000000", family: "Arial" }
+    };
     
-    // Plotly.newPlot("gauge", data3, layout3);
-        
+    //Plotly.newPlot("gauge", data3, layout3);
   });
 }
 
 
 // ***** Metadata Table *****
-// Display the sample metadata, i.e., an individual's demographic information.
-// Display each key-value pair from the metadata JSON object somewhere on the page.
-// Update all of the plots any time that a new sample is selected.
-
 function getData(id) {
   // read the json data 
   d3.json("samples.json").then((data) => {  
     
     var metaData = data.metadata;
-    
     console.log(metaData);
   
     // filter meta data info by id
@@ -197,6 +187,7 @@ function optionChanged(id) {
   getData(id);
 }
 
+
 // ***** Dropdown Menu ******
 
 // create the function for the initial data rendering
@@ -221,3 +212,5 @@ function init() {
 }
 
 init();
+
+
